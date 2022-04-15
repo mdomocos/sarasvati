1bbc71590a60bc32283de9bf0f046830d0a038c1
Mihai Domocos (ATOS) <84894289+A687265@users.noreply.github.com>
2022-04-15 09:48:38 +0300

    Create sarasvati.ps1

---
+++ sarasvati.ps1
@@ -0,0 +1,11 @@
+$P = Get-Process
+
+Read-Host -Prompt "Press Enter to start Sarasvati"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Write-Output "Remove hanging docker.pid file"
+del C:\ProgramData\docker\docker.pid
+Start-Process ".\docker-20.10.14\docker\dockerd.exe" -ArgumentList "-D --experimental" -WindowStyle Hidden
+Write-Output "Wait 15 seconds for dockerd to start. Then run the containers"
+Start-Sleep -s 15
+.\docker-compose.exe -f stt-ukr-docker-compose.yml up -d
+Read-Host -Prompt "Press Enter to exit"
