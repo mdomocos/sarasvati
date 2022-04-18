b66bf79bb716a98ef88abed180855c4a7d5f3975
Mihai Domocos <mihaidomocos@gmail.com>
2022-04-18 17:00:19 +0300

    updates

--- sarasvati.ps1
+++ sarasvati.ps1
@@ -1,13 +1,8 @@
-$P = Get-Process
-
-Read-Host -Prompt "Press Enter to start Sarasvati"
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Write-Output "Remove hanging docker.pid file"
-del C:\ProgramData\docker\docker.pid
 Start-Process ".\docker-20.10.14\docker\dockerd.exe" -ArgumentList "-D --experimental" -WindowStyle Hidden
 Write-Output "Wait 15 seconds for dockerd to start. Then run the containers"
 Start-Sleep -s 15
-.\docker-compose.exe -f stt-ukr-docker-compose.yml up -d
+Start-Process ".\docker-20.10.14\docker\docker.exe" -ArgumentList "system prune -af" -WindowStyle Hidden
+.\docker-compose.exe -f stt-ukr-docker-compose.yml up -d 
 .\docker-compose.exe -f entities-extractor-xlu-docker-compose.yml up -d
 .\docker-compose.exe -f sarasvati-docker-compose.yml up -d
 Read-Host -Prompt "Press Enter to exit"
