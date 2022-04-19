768b5a588e282d4865516bf0625e5623422b7f22
Mihai Domocos <mihaidomocos@gmail.com>
2022-04-19 08:11:48 +0300

    Added pid deletion for docker

--- sarasvati.ps1
+++ sarasvati.ps1
@@ -1,3 +1,6 @@
+Write-Output "Remove hanging docker.pid file"
+del C:\ProgramData\docker\docker.pid
+Write-Output "Starting dockerd"
 Start-Process ".\docker-20.10.14\docker\dockerd.exe" -ArgumentList "-D --experimental" -WindowStyle Hidden
 Write-Output "Wait 15 seconds for dockerd to start. Then run the containers"
 Start-Sleep -s 15
