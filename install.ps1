d214a607fc673b41e3ccbc4f3417abe2a28d9a14
Mihai Domocos <mihaidomocos@gmail.com>
2022-04-27 09:10:56 +0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -3,8 +3,9 @@ Read-Host -Prompt "Press Enter to install Sarasvati. Make sure you run this scri
 Set-ExecutionPolicy RemoteSigned
 Write-Output "Enabling Containers Feature - Do not accept the restart."
 Enable-WindowsOptionalFeature -Online -FeatureName containers -All
-Write-Output "Enabling Hyper-V Feature - When asked, restart your computer. Run this script again after the restart."
+Write-Output "Enabling Hyper-V Feature - Do not accept the restart."
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+Write-Output "Enabling Windows Subsystem Linux - Do not accept the restart."
 wsl --install
 
 Invoke-WebRequest -Uri "https://github.com/linuxkit/lcow/releases/download/v4.14.35-v0.3.9/release.zip" -UseBasicParsing -OutFile release.zip
@@ -15,4 +16,5 @@ Invoke-WebRequest https://download.docker.com/win/static/stable/x86_64/docker-20
 Expand-Archive docker-20.10.14.zip
 
 Invoke-WebRequest https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Windows-x86_64.exe -UseBasicParsing -OutFile docker-compose.exe
-Read-Host -Prompt "Press Enter to exit"
+Read-Host -Prompt "Installation is now complete, press ENTER to restart your computer."
+Restart-Computer
